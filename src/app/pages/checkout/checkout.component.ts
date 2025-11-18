97ef260a87f8e21208e9125da42acb7031041608
aumtuhin <dev.aumtuhin@gmail.com>
2025-11-18 15:34:16 +0100

    feat: removed unused buttom in checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
-import { ButtonComponent } from '../../components/ui/button/button.component';
 import { ProductListComponent } from '../../components/product-list/product-list.component';
 import { CartComponent } from '../../components/cart/cart.component';
 
 @Component({
   selector: 'app-checkout',
-  imports: [ButtonComponent, ProductListComponent, CartComponent],
+  imports: [ProductListComponent, CartComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
